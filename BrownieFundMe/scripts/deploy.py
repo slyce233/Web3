faf1d3996160a02cfd9d5efe8f19f99f218ab274
slyce24 <andrew.adamah@ontariotechu.net>
2022-04-09 16:57:25 -0400

    added support for ganache local

--- BrownieFundMe/scripts/deploy.py
+++ BrownieFundMe/scripts/deploy.py
@@ -1,19 +1,20 @@
 from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helper_script import get_account, deploy_mocks
 from web3 import Web3
+import sys
 
 
 def deploy_fund_me():
     account = get_account()
 
     # check for network and deploy address or mock
-    if network.show_active() != "development":
+    if network.show_active() == "development" or sys.argv[-1] == "ganache-local":
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+    else:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
-    else:
-        deploy_mocks()
-        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
--- BrownieFundMe/scripts/helper_script.py
+++ BrownieFundMe/scripts/helper_script.py
@@ -1,12 +1,13 @@
 from brownie import MockV3Aggregator, accounts, network, config
 from web3 import Web3
+import sys
 
 DECIMALS = 18
 STARTING_PRICE = Web3.toWei(2000, "ether")
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() == "development" or sys.argv[-1] == "ganache-local":
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
